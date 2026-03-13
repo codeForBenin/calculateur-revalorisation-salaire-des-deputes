415a486c092b11973d76dcfc978318f8ade4e251
Esdras Onionkiton <esdrasonionkiton@gmail.com>
2026-03-13 14:39:15 +0100

    feat: add Vercel Analytics integration

--- app/root.tsx
+++ app/root.tsx
@@ -7,7 +7,7 @@ import {
   ScrollRestoration,
 } from "react-router";
 import { SpeedInsights } from "@vercel/speed-insights/react"
-
+import { Analytics } from "@vercel/analytics/react"
 import type { Route } from "./+types/root";
 import "./app.css";
 
@@ -72,6 +72,7 @@ export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
         </pre>
       )}
       <SpeedInsights />
+      <Analytics />
     </main>
   );
 }
